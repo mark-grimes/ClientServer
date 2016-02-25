12342dea00d607af3785219f38e5bbdcb0c142e4
Mark Grimes <kknb1056@gmail.com>
2016-02-25 10:49:11 +0000

    Add option to specify which port number the server should listen on

--- src/server/commands/listen.cpp
+++ src/server/commands/listen.cpp
@@ -7,6 +7,7 @@ public:
 };
 
 #include "tools/SubExecutableRegister.h"
+#include "tools/CommandLineParser.h"
 #include <communique/Server.h>
 #include <iostream>
 #include <mutex>
@@ -16,6 +17,69 @@ REGISTER_MODULE( ListenSubExe, "listen" );
 
 int ListenSubExe::run( int argc, char* argv[] )
 {
+	size_t portNumber=9002;
+
+	//
+	// Try and parse the command line arguments
+	//
+	try
+	{
+		tools::CommandLineParser commandLineParser;
+		commandLineParser.addOption( "help", tools::CommandLineParser::NoArgument );
+		commandLineParser.addOption( "port", tools::CommandLineParser::RequiredArgument );
+
+		commandLineParser.parse( argc, argv );
+
+		if( commandLineParser.optionHasBeenSet("help") )
+		{
+			std::cout << "Usage:" << "\n"
+					  << "  " << commandLineParser.executableName() << " [command options]" << "\n"
+					  << "\n"
+					  << "Available options:" << "\n"
+					  << "  --help      Display this help message and exit" << "\n"
+					  << "  --port      The port number for the server to listen on. Default is " << portNumber << "." << "\n"
+					  << std::endl;
+			return 0;
+		}
+
+		if( commandLineParser.optionHasBeenSet("port") )
+		{
+			std::string numberAsString;
+			try
+			{
+				numberAsString=commandLineParser.optionArguments("port").back();
+				size_t pos;
+				int numberAsInt=std::stoi(numberAsString,&pos);
+				if( pos!=numberAsString.size() ) throw std::runtime_error( "the string was not fully converted" );
+				if( numberAsInt<0 ) throw std::runtime_error( "the port number is negative" );
+
+				portNumber=numberAsInt;
+			}
+			catch( std::invalid_argument& error )
+			{
+				std::cerr << "Couldn't parse the argument for --port (\"" << numberAsString << "\") because it could not be converted to an integer. Only specify positive integers." << std::endl;
+				return -1;
+			}
+			catch( std::out_of_range& error )
+			{
+				std::cerr << "Couldn't parse the argument for --port (\"" << numberAsString << "\") because the number was out of range. Only specify positive integers in the valid range." << std::endl;
+				return -1;
+			}
+			catch( std::exception& error )
+			{
+				std::cerr << "Couldn't parse the argument for --port (\"" << numberAsString << "\") because " << error.what() << ". Only specify positive integers." << std::endl;
+				return -1;
+			}
+		} // end of "port" option check
+	} // end of parsing arguments try block
+	catch( std::exception& error )
+	{
+		std::cerr << "The following error was encountered while parsing the command line:" << "\n"
+		          << "     " << error.what() << "\n"
+				  << "Try \"--help\" for usage instructions." << std::endl;
+		return -1;
+	}
+
 	// The synchronisation variables required to decide when to quit
 	bool continueListening=true;
 	std::mutex continueListeningMutex;
@@ -43,7 +107,8 @@ int ListenSubExe::run( int argc, char* argv[] )
 		});
 
 	// Start listening...
-	commandServer.listen(9002);
+	std::cout << "Starting to listen on port " << portNumber << std::endl;
+	commandServer.listen(portNumber);
 	// ...and wait until told to stop
 	std::unique_lock<std::mutex> lock(continueListeningMutex);
 	continueListeningCondition.wait( lock, [&]{ return !continueListening; } );
